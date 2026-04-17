0d5a5f57a9eeab381aea18bf24529a5ab3577c3f
Sevastyan Chernykh <sseva2892@gmail.com>
2026-04-17 10:34:18 +0000

    удалил объявление неиспользуемой функции в файле ops.seq.hpp

--- tasks/chernykh_s_trapezoidal_integration/seq/include/ops_seq.hpp
+++ tasks/chernykh_s_trapezoidal_integration/seq/include/ops_seq.hpp
@@ -14,13 +14,6 @@ class ChernykhSTrapezoidalIntegrationSEQ : public BaseTask {
   explicit ChernykhSTrapezoidalIntegrationSEQ(const InType &in);
 
  private:
-  void RecursiveMethod(size_t dim, std::vector<double> &current_point, double current_coeff,
-                       const IntegrationInType &input, double &total_sum);
-  // size_t dim - индекс оси, по какой мы идем
-  // std::vector<double>& current_point - сюда записываем вычисленные координаты
-  // double current_coeff - вес для текущей точки
-  // const IntegrationInType& Input - входные данные
-  // double& total_sum - копилка сумм
 
   static double CalculatePointAndWeight(const IntegrationInType &input, const std::vector<std::size_t> &counters,
                                         std::vector<double> &point);
