ff4daa1447ba9b22ff6683e2f9f59b3bffe516b9
Sevastyan Chernykh <sseva2892@gmail.com>
2026-05-15 13:33:29 +0300

    пре-коммит

--- tasks/chernykh_s_trapezoidal_integration/stl/src/ops_stl.cpp
+++ tasks/chernykh_s_trapezoidal_integration/stl/src/ops_stl.cpp
@@ -1,14 +1,13 @@
 #include "chernykh_s_trapezoidal_integration/stl/include/ops_stl.hpp"
 
-
 #include <algorithm>
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
 #include <functional>
+#include <thread>
 #include <utility>
 #include <vector>
-#include <thread>
 
 #include "chernykh_s_trapezoidal_integration/common/include/common.hpp"
 
@@ -49,49 +48,47 @@ double ChernykhSTrapezoidalIntegrationSTL::CalculatePointAndWeight(const Integra
   return weight;
 }
 
-
-
-
-
 bool ChernykhSTrapezoidalIntegrationSTL::RunImpl() {
-  auto & input= GetInput();
-  int dims = input.limits.size(); // количество измерений
+  auto &input = GetInput();
+  int dims = input.limits.size();  // количество измерений
   int64_t total_point = 1;
-  for(int setka: input.steps){
-    total_point *= static_cast<int64_t>(setka)+1;
+  for (int setka : input.steps) {
+    total_point *= static_cast<int64_t>(setka) + 1;
   }
 
   int num_threads = ppc::util::GetNumThreads();
-  if (num_threads ==0) num_threads=1;
-  std::cout<<num_threads<<std::endl;
+  if (num_threads == 0) {
+    num_threads = 1;
+  }
+  std::cout << num_threads << std::endl;
 
-  std::vector<double> result(num_threads, 0.0); // вектор, в котором будут храниться результаты
-  std::vector<std::thread> threads(num_threads); // вектор потоков 
-  std::vector<std::pair<int64_t,int64_t>> borders(num_threads);
+  std::vector<double> result(num_threads, 0.0);   // вектор, в котором будут храниться результаты
+  std::vector<std::thread> threads(num_threads);  // вектор потоков
+  std::vector<std::pair<int64_t, int64_t>> borders(num_threads);
 
   int64_t points_per_thread = total_point / num_threads;
   int64_t remainder = total_point % num_threads;
 
   int64_t start = 0;
-  for(int i=0; i <num_threads;i++){ // 11 3 по 3 остаток 2 : [0,3+1), [4,8), [8,11)
+  for (int i = 0; i < num_threads; i++) {  // 11 3 по 3 остаток 2 : [0,3+1), [4,8), [8,11)
     borders[i].first = start;
-    start+=points_per_thread;
-    if(i<remainder){
+    start += points_per_thread;
+    if (i < remainder) {
       start++;
     }
     borders[i].second = start;
   }
-  auto WorkFunction = [&](int64_t start, int64_t end, double& local_result){
+  auto WorkFunction = [&](int64_t start, int64_t end, double &local_result) {
     local_result = 0.0;
     std::vector<double> point(dims);
-    for(int64_t i = start; i < end;i++){
+    for (int64_t i = start; i < end; i++) {
       // Метод трапеций : вес(точка)*f(точка)
       int64_t local_index = i;
-      std::vector<size_t> grid_index(dims,0); // для хранения идексов точек
-      
-      for( int j = dims-1; j>=0;j--){ // считаем индекс
-        grid_index[j] = local_index % (input.steps[j]+1);
-        local_index /= input.steps[j]+1;
+      std::vector<size_t> grid_index(dims, 0);  // для хранения идексов точек
+
+      for (int j = dims - 1; j >= 0; j--) {  // считаем индекс
+        grid_index[j] = local_index % (input.steps[j] + 1);
+        local_index /= input.steps[j] + 1;
       }
       double weight = CalculatePointAndWeight(input, grid_index, point);
       local_result += input.func(point) * weight;
@@ -101,20 +98,19 @@ bool ChernykhSTrapezoidalIntegrationSTL::RunImpl() {
     threads[i] = std::thread(WorkFunction, borders[i].first, borders[i].second, std::ref(result[i]));
   }
 
-  
-for (int i = 0; i < num_threads; ++i) {
+  for (int i = 0; i < num_threads; ++i) {
     threads[i].join();
   }
   double output_result = 0.0;
-  for(double loc_res: result){
-output_result+=loc_res;
+  for (double loc_res : result) {
+    output_result += loc_res;
   }
 
-  for(int i=0;i<dims;i++){
+  for (int i = 0; i < dims; i++) {
     double h = (input.limits[i].second - input.limits[i].first) / input.steps[i];
-    output_result*=h;
-    }
-  
+    output_result *= h;
+  }
+
   GetOutput() = output_result;
   return true;
 }
