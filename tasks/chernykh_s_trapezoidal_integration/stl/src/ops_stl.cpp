4d51e33ad1272ddd910149e5639915719bb68e76
Sevastyan Chernykh <sseva2892@gmail.com>
2026-05-15 13:52:51 +0300

    добавил локльную сумму для потока

--- tasks/chernykh_s_trapezoidal_integration/stl/src/ops_stl.cpp
+++ tasks/chernykh_s_trapezoidal_integration/stl/src/ops_stl.cpp
@@ -1,99 +1,115 @@
-#include "chernykh_s_trapezoidal_integration/stl/include/ops_stl.hpp"
+  #include "chernykh_s_trapezoidal_integration/stl/include/ops_stl.hpp"
 
-#include <algorithm>
-#include <cmath>
-#include <cstddef>
-#include <cstdint>
-#include <functional>
-#include <thread>
-#include <utility>
-#include <vector>
 
-#include "chernykh_s_trapezoidal_integration/common/include/common.hpp"
+  #include <algorithm>
+  #include <cmath>
+  #include <cstddef>
+  #include <cstdint>
+  #include <functional>
+  #include <utility>
+  #include <vector>
+  #include <thread>
 
-namespace chernykh_s_trapezoidal_integration {
+  #include "chernykh_s_trapezoidal_integration/common/include/common.hpp"
 
-ChernykhSTrapezoidalIntegrationSTL::ChernykhSTrapezoidalIntegrationSTL(const InType &in) {
-  SetTypeOfTask(GetStaticTypeOfTask());
-  GetInput() = in;
-  GetOutput() = 0.0;
-}
+  namespace chernykh_s_trapezoidal_integration {
 
-bool ChernykhSTrapezoidalIntegrationSTL::ValidationImpl() {
-  const auto &input = this->GetInput();
-  if (input.limits.empty() || input.limits.size() != input.steps.size()) {
-    return false;
+  ChernykhSTrapezoidalIntegrationSTL::ChernykhSTrapezoidalIntegrationSTL(const InType &in) {
+    SetTypeOfTask(GetStaticTypeOfTask());
+    GetInput() = in;
+    GetOutput() = 0.0;
   }
-  return std::ranges::all_of(input.steps, [](int s) { return s > 0; });
-}
 
-bool ChernykhSTrapezoidalIntegrationSTL::PreProcessingImpl() {
-  return true;
-}
-
-double ChernykhSTrapezoidalIntegrationSTL::CalculatePointAndWeight(const IntegrationInType &input,
-                                                                   const std::vector<std::size_t> &counters,
-                                                                   std::vector<double> &point) {
-  double weight = 1.0;
-  for (std::size_t i = 0; i < input.limits.size(); ++i) {  // проходим по границам каждого из измерений
-    const double h = (input.limits[i].second - input.limits[i].first) /
-                     static_cast<double>(input.steps[i]);  // велечина шага в текущем измерении
-    point[i] = input.limits[i].first +
-               (static_cast<double>(counters[i]) * h);  // дискретная точка: начало_измерения + шаг*номер_шага
-    if (std::cmp_equal(counters[i], 0) ||
-        std::cmp_equal(counters[i], input.steps[i])) {  // если шаг граничный, то его вес уменьшается
-      weight *= 0.5;
+  bool ChernykhSTrapezoidalIntegrationSTL::ValidationImpl() {
+    const auto &input = this->GetInput();
+    if (input.limits.empty() || input.limits.size() != input.steps.size()) {
+      return false;
     }
+    return std::ranges::all_of(input.steps, [](int s) { return s > 0; });
   }
-  return weight;
-}
 
-bool ChernykhSTrapezoidalIntegrationSTL::RunImpl() {
-  auto &input = GetInput();
-  int dims = input.limits.size();  // количество измерений
+  bool ChernykhSTrapezoidalIntegrationSTL::PreProcessingImpl() {
+    return true;
+  }
+
+  double ChernykhSTrapezoidalIntegrationSTL::CalculatePointAndWeight(const IntegrationInType &input,
+                                                                    const std::vector<std::size_t> &counters,
+                                                                    std::vector<double> &point) {
+    double weight = 1.0;
+    for (std::size_t i = 0; i < input.limits.size(); ++i) {  // проходим по границам каждого из измерений
+      const double h = (input.limits[i].second - input.limits[i].first) /
+                      static_cast<double>(input.steps[i]);  // велечина шага в текущем измерении
+      point[i] = input.limits[i].first +
+                (static_cast<double>(counters[i]) * h);  // дискретная точка: начало_измерения + шаг*номер_шага
+      if (std::cmp_equal(counters[i], 0) ||
+          std::cmp_equal(counters[i], input.steps[i])) {  // если шаг граничный, то его вес уменьшается
+        weight *= 0.5;
+      }
+    }
+    return weight;
+  }
+
+
+
+
+
+  bool ChernykhSTrapezoidalIntegrationSTL::RunImpl() {
+  auto & input = GetInput();
+  int dims = input.limits.size();
   int64_t total_point = 1;
-  for (int setka : input.steps) {
+  for(int setka: input.steps){
     total_point *= static_cast<int64_t>(setka) + 1;
   }
 
-  int num_threads = ppc::util::GetNumThreads();
-  if (num_threads == 0) {
-    num_threads = 1;
+  std::vector<double> h(dims);
+  for (int i = 0; i < dims; ++i) {
+    h[i] = (input.limits[i].second - input.limits[i].first) / static_cast<double>(input.steps[i]);
   }
-  std::cout << num_threads << std::endl;
 
-  std::vector<double> result(num_threads, 0.0);   // вектор, в котором будут храниться результаты
-  std::vector<std::thread> threads(num_threads);  // вектор потоков
+  int num_threads = ppc::util::GetNumThreads();
+  if (num_threads == 0) num_threads = 1;
+
+  std::vector<double> result(num_threads, 0.0); 
+  std::vector<std::thread> threads(num_threads); 
   std::vector<std::pair<int64_t, int64_t>> borders(num_threads);
 
   int64_t points_per_thread = total_point / num_threads;
   int64_t remainder = total_point % num_threads;
 
   int64_t start = 0;
-  for (int i = 0; i < num_threads; i++) {  // 11 3 по 3 остаток 2 : [0,3+1), [4,8), [8,11)
+  for(int i = 0; i < num_threads; i++){ 
     borders[i].first = start;
     start += points_per_thread;
-    if (i < remainder) {
+    if(i < remainder){
       start++;
     }
     borders[i].second = start;
   }
-  auto WorkFunction = [&](int64_t start, int64_t end, double &local_result) {
-    local_result = 0.0;
-    std::vector<double> point(dims);
-    for (int64_t i = start; i < end; i++) {
-      // Метод трапеций : вес(точка)*f(точка)
-      int64_t local_index = i;
-      std::vector<size_t> grid_index(dims, 0);  // для хранения идексов точек
 
-      for (int j = dims - 1; j >= 0; j--) {  // считаем индекс
-        grid_index[j] = local_index % (input.steps[j] + 1);
-        local_index /= input.steps[j] + 1;
+  auto WorkFunction = [&](int64_t start, int64_t end, double& local_result) {
+  double sum = 0.0; 
+  std::vector<double> point(dims);
+
+  for (int64_t i = start; i < end; i++) {
+    int64_t local_index = i;
+    double weight = 1.0;
+    
+    for (int j = dims - 1; j >= 0; j--) { 
+      int64_t idx = local_index % (input.steps[j] + 1);
+      local_index /= (input.steps[j] + 1);
+
+      point[j] = input.limits[j].first + (static_cast<double>(idx) * h[j]);
+
+      if (idx == 0 || idx == input.steps[j]) {
+        weight *= 0.5;
       }
-      double weight = CalculatePointAndWeight(input, grid_index, point);
-      local_result += input.func(point) * weight;
     }
-  };
+    sum += input.func(point) * weight; 
+  }
+
+  local_result = sum; 
+};
+
   for (int i = 0; i < num_threads; ++i) {
     threads[i] = std::thread(WorkFunction, borders[i].first, borders[i].second, std::ref(result[i]));
   }
@@ -101,22 +117,22 @@ bool ChernykhSTrapezoidalIntegrationSTL::RunImpl() {
   for (int i = 0; i < num_threads; ++i) {
     threads[i].join();
   }
+
   double output_result = 0.0;
-  for (double loc_res : result) {
+  for(double loc_res: result){
     output_result += loc_res;
   }
 
-  for (int i = 0; i < dims; i++) {
-    double h = (input.limits[i].second - input.limits[i].first) / input.steps[i];
-    output_result *= h;
+  for(int i = 0; i < dims; i++){
+    output_result *= h[i];
   }
-
+    
   GetOutput() = output_result;
   return true;
 }
 
-bool ChernykhSTrapezoidalIntegrationSTL::PostProcessingImpl() {
-  return true;
-}
+  bool ChernykhSTrapezoidalIntegrationSTL::PostProcessingImpl() {
+    return true;
+  }
 
-}  // namespace chernykh_s_trapezoidal_integration
+  }  // namespace chernykh_s_trapezoidal_integration
