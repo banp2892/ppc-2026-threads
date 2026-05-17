fbea1787101eae8e8638cf0e354b0036498c6cfe
Sevastyan Chernykh <sseva2892@gmail.com>
2026-05-17 15:15:08 +0300

    удалил stl тесты из тестов)

--- tasks/chernykh_s_trapezoidal_integration/tests/performance/main.cpp
+++ tasks/chernykh_s_trapezoidal_integration/tests/performance/main.cpp
@@ -53,8 +53,6 @@ const auto kAllPerfTasks = std::tuple_cat(ppc::util::MakeAllPerfTasks<InType, Ch
                                               PPC_SETTINGS_chernykh_s_trapezoidal_integration),
                                           ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationTBB>(
                                               PPC_SETTINGS_chernykh_s_trapezoidal_integration),
-                                          ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationSTL>(
-                                              PPC_SETTINGS_chernykh_s_trapezoidal_integration),
                                           ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationALL>(
                                               PPC_SETTINGS_chernykh_s_trapezoidal_integration));
 
