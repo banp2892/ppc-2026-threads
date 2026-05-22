6c27e59274924da32236ed679151e645dfd5edc2
Sevastyan Chernykh <sseva2892@gmail.com>
2026-05-22 19:21:09 +0300

    добавил отчеты

--- tasks/chernykh_s_trapezoidal_integration/tests/performance/main.cpp
+++ tasks/chernykh_s_trapezoidal_integration/tests/performance/main.cpp
@@ -48,16 +48,16 @@ TEST_P(ChernykhSRunPerfTestThreads, RunPerfModes) {
 
 namespace {
 
-const auto kAllPerfTasks = std::tuple_cat(ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationSEQ>(
+const auto kAllPerfTasks = std::tuple_cat(/*ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationSEQ>(
                                               PPC_SETTINGS_chernykh_s_trapezoidal_integration),
                                           ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationOMP>(
                                               PPC_SETTINGS_chernykh_s_trapezoidal_integration),
                                           ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationTBB>(
                                               PPC_SETTINGS_chernykh_s_trapezoidal_integration),
                                           ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationSTL>(
-                                              PPC_SETTINGS_chernykh_s_trapezoidal_integration),
+                                              PPC_SETTINGS_chernykh_s_trapezoidal_integration),*/
                                           ppc::util::MakeAllPerfTasks<InType, ChernykhSTrapezoidalIntegrationALL>(
-                                              PPC_SETTINGS_chernykh_s_trapezoidal_integration));
+                                              PPC_SETTINGS_chernykh_s_trapezoidal_integration)/**/);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
